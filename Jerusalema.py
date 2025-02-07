a0e4f137e555db536eb60f4a6b98ff837b11161b
KennethTebogo <168050607+KennethTebogo@users.noreply.github.com>
2025-02-07 03:05:19 +0200

    Add files via upload

---
+++ Jerusalema.py
@@ -0,0 +1,20 @@
+# CHPC Summer School 2025
+# Probability & Statistics: Categorical Data & Probability
+# Example: Jerusalema
+
+# Import packages and functions
+
+import numpy as np
+from scipy.stats import chi2_contingency
+
+# Give the contingency table with observed counts			
+
+ctJerusalema = np.array([[28, 12], [16, 24]])
+
+# Do the test for independence
+
+chi2_contingency(ctJerusalema)
+
+result = chi2_contingency(ctJerusalema)
+
+print(result)
